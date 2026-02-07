9515eedbaa446cd1e8316c30a3110c2397ab20d5
0xGuigui <guilhem.jeh@gmail.com>
2026-02-07 20:31:52 +0100

    feat: create Archives page with WorkspaceShell layout

---
+++ client/src/pages/archives.tsx
@@ -0,0 +1,11 @@
+import { WorkspaceShell } from "../components/workspace-shell"
+
+function Archives() {
+  return (
+    <WorkspaceShell>
+      <h1 className="sr-only">Archives</h1>
+    </WorkspaceShell>
+  )
+}
+
+export default Archives
