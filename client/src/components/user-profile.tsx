6c22c727566a63badb23c99001202ab7625a2b82
dorydev <95535428+dorydev@users.noreply.github.com>
2026-02-09 22:41:15 +0100

    create component

---
+++ client/src/components/user-profile.tsx
@@ -0,0 +1,13 @@
+import { Popover } from "./ui/popover"
+
+export function userProfile() {
+
+  return (
+
+    <Popover>
+      <PopoverTrigger asChild>
+
+      </PopoverTrigger>
+    </Popover>
+  )
+}
